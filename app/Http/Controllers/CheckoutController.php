a218e17dcd5e6d1f4566bcbefd66d0a53d85fe6f
Nazmulon <nazmulon0@gmail.com>
2019-08-01 19:29:59 +0600

    Update CheckoutController.php

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -131,7 +131,15 @@ class CheckoutController extends Controller
     }
 
     public function view_order(){
-        return view('admin.view_order');
+        $order_by_id=DB::table('tbl_order')
+                    ->join('tbl_customer', 'tbl_order.customer_id', '=', 'tbl_customer.customer_id')
+                    ->join('tbl_order_details', 'tbl_order.order_id', '=', 'tbl_order_details.order_id')
+                    ->join('tbl_shipping', 'tbl_order.shipping_id', 'tbl_shipping.shipping_id')
+                    ->select('tbl_order.*', 'tbl_order_details.*', 'tbl_shipping.*', 'tbl_customer.*')
+                    ->first();
+
+            $view_order=view('admin.view_order')->with('order_by_id', $order_by_id);
+            return view('admin_layout')->with('admin.view_order', $view_order);
     }
 
     public function customer_logout(){
